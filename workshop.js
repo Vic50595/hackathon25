de00ad682294c2d000c65a667c730348cbd9e21b
Victoria P <victoria@test.be>
2025-11-14 09:59:47 +0100

    last update workshop.html / css + pdf

--- workshop.js
+++ workshop.js
@@ -1,6 +1,6 @@
   document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
-    link.href = 'files/myfile.pdf'; // path to your PDF
+    link.href = 'images/paygap.pdf'; // path to your PDF
     link.download = 'myfile.pdf';   // filename for download
     document.body.appendChild(link);
     link.click();
